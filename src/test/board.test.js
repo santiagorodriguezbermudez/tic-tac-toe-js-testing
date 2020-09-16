4e6a64a2569c25edc28f828dba9f3bb4d642fe86
LoboArkano <pipiolo_mananes@hotmail.com>
2020-09-16 15:38:30 -0500

    Add test for board.test.js

---
+++ src/test/board.test.js
@@ -0,0 +1,13 @@
+import GameBoard from '../js/board'
+
+
+test('getMovesArray return an empty array', () => {
+  GameBoard.resetMovesArray();
+  expect(GameBoard.getMovesArray()).toStrictEqual(['', '', '', '', '', '', '', '', '']);
+});
+
+test('updateMovesArray update movesArray with the player token', () => {
+  GameBoard.resetMovesArray();
+  GameBoard.updateMovesArray(5,'X');
+  expect(GameBoard.getMovesArray()).toStrictEqual(['', '', '', '', '', 'X', '', '', '']);
+});
